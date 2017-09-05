6d2c206d27b9136fe450e62060786efa85b93bcb
shoiphy <shoiphy@gmail.com>
2017-09-05 10:16:22 +0800

    适应php5.3以上版本
    PHP5.3版本开始，array_shift不支持函数返回，将422行的$tag_sel = array_shift(explode(' ', $tag));
    将其注释掉，分别拆开，添加两行
    $tag_tmp = (explode(' ', $tag));
    $tag_sel = array_shift($tag_tmp);

--- upload/includes/cls_template.php
+++ upload/includes/cls_template.php
@@ -419,7 +419,9 @@ class cls_template
         }
         else
         {
-            $tag_sel = array_shift(explode(' ', $tag));
+            //$tag_sel = array_shift(explode(' ', $tag));
+            $tag_tmp = (explode(' ', $tag));
+            $tag_sel = array_shift($tag_tmp);
             switch ($tag_sel)
             {
                 case 'if':
