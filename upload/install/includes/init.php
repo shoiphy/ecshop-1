647a8b2711479af529c19ce7f1bde5afbfc19897
shoiphy <shoiphy@gmail.com>
2017-09-05 12:22:50 +0800

    适应php7
    适应php7,添加使用mysqli方法替换mysql方法

--- upload/install/includes/init.php
+++ upload/install/includes/init.php
@@ -1,13 +1,13 @@
 <?php
 
-/* ±¨¸æËùÓÐ´íÎó */
+/* æŠ¥å‘Šæ‰€æœ‰é”™è¯¯ */
 @ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
-/* Çå³ýËùÓÐºÍÎÄ¼þ²Ù×÷Ïà¹ØµÄ×´Ì¬ÐÅÏ¢ */
+/* æ¸…é™¤æ‰€æœ‰å’Œæ–‡ä»¶æ“ä½œç›¸å…³çš„çŠ¶æ€ä¿¡æ¯ */
 clearstatcache();
 
-/* ¶¨ÒåÕ¾µã¸ù */
+/* å®šä¹‰ç«™ç‚¹æ ¹ */
 define('ROOT_PATH', str_replace('install/includes/init.php', '', str_replace('\\', '/', __FILE__)));
 
 if (isset($_SERVER['PHP_SELF']))
@@ -19,7 +19,7 @@ else
     define('PHP_SELF', $_SERVER['SCRIPT_NAME']);
 }
 
-/* ¶¨Òå°æ±¾µÄ±àÂë */
+/* å®šä¹‰ç‰ˆæœ¬çš„ç¼–ç  */
 define('EC_CHARSET','utf-8');
 define('EC_DB_CHARSET','utf8');
 
@@ -27,17 +27,20 @@ require(ROOT_PATH . 'includes/lib_base.php');
 require(ROOT_PATH . 'includes/lib_common.php');
 require(ROOT_PATH . 'includes/lib_time.php');
 
-/* ´´½¨´íÎó´¦Àí¶ÔÏó */
+/* åˆ›å»ºé”™è¯¯å¤„ç†å¯¹è±¡ */
 require(ROOT_PATH . 'includes/cls_error.php');
 $err = new ecs_error('message.dwt');
 
-/* ³õÊ¼»¯Ä£°åÒýÇæ */
+/* é€‚åº”php7,ä½¿ç”¨mysqliæ–¹æ³•æ›¿æ¢mysqlæ–¹æ³• */
+require(ROOT_PATH . 'includes/tomysqli.php');
+
+/* åˆå§‹åŒ–æ¨¡æ¿å¼•æ“Ž */
 require(ROOT_PATH . 'install/includes/cls_template.php');
 $smarty = new template(ROOT_PATH . 'install/templates/');
 
 require(ROOT_PATH . 'install/includes/lib_installer.php');
 
-/* ·¢ËÍHTTPÍ·²¿£¬±£Ö¤ä¯ÀÀÆ÷Ê¶±ðUTF8±àÂë */
+/* å‘é€HTTPå¤´éƒ¨ï¼Œä¿è¯æµè§ˆå™¨è¯†åˆ«UTF8ç¼–ç  */
 header('Content-type: text/html; charset='.EC_CHARSET);
 
 @set_time_limit(360);
